15fa5beb8d2c2c38627312e92c527be9c2aedc96
mmiha01 <m.mihaljevic97@gmail.com>
2019-05-17 13:59:22 +0200

    Added pause,play,next,prev

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Player } from './player'
-const logoMedium = require('./logo-medium.png')
+const logoMedium = require('./images/logo-medium.png')
 
 export interface MainProps { compiler: string; framework: string; }
 
@@ -17,12 +17,16 @@ interface State {
     duration: number,
     progress: number,
     gotPlayerInfo: boolean,
+    volume: number,
+    repeatMode: string,
+    shuffle: boolean,
 }
 
 interface DevicesList {
     id: number,
     is_active: boolean,
     name: string,
+    volume_percent: number,
  }
 
 export class Main extends React.Component<MainProps, State, DevicesList> {
@@ -39,12 +43,15 @@ export class Main extends React.Component<MainProps, State, DevicesList> {
         artists: '',
         duration: 0,
         progress: 0,
-        gotPlayerInfo: false
+        gotPlayerInfo: false,
+        volume: 0,
+        repeatMode: '',
+        shuffle: false,
     }
 
     redirectToLogin = () => {
         // tslint:disable-next-line: max-line-length
-        const scopes = encodeURIComponent('user-read-private user-read-email user-read-playback-state user-read-currently-playing')
+        const scopes = encodeURIComponent('user-read-private user-read-email user-read-playback-state user-read-currently-playing user-modify-playback-state')
         const redirectURL = encodeURIComponent('http://localhost:3000/auth')
         const clientID = '156f1e1dec944ef78fb5d38131610afb'
         const spotifyAuth = 'https://accounts.spotify.com/authorize?response_type=token' +
@@ -89,8 +96,8 @@ export class Main extends React.Component<MainProps, State, DevicesList> {
                 'Authorization': 'Bearer ' + this.getTokenFromCookie()
             }
         }).then((res) => res.text()).then((res) => {
-            const resData = JSON.parse(res)
-            if (resData.error) {
+            const resData = res.length > 0 ? JSON.parse(res) : null
+            if (resData && resData.error) {
                 this.setState({ isAuthorized: false })
                 this.setState({ didCheckAuthState: true })
                 this.setState({ gotPlayerInfo: true })
@@ -119,6 +126,27 @@ export class Main extends React.Component<MainProps, State, DevicesList> {
         }
     }
 
+    getShuffleAndRepeat = (allow: boolean = true) => {
+        if (!allow) {
+            return false
+        }
+        return fetch(`https://api.spotify.com/v1/me/player/`, {
+            headers: {
+                'Authorization': 'Bearer ' + this.getTokenFromCookie()
+            }
+        }).then((res) => res.text()).then((res) => {
+            const resData = res.length > 0 ? JSON.parse(res) : null
+            if (resData && resData.error) {
+                this.setState({ isAuthorized: false })
+                this.setState({ gotPlayerInfo: true })
+                return false
+            }
+            this.setState({repeatMode: resData.repeat_state})
+            this.setState({shuffle: resData.shuffle_state})
+            return true
+        })
+    }
+
     getDevice = (allow: boolean = true) => {
         if (!allow) {
             return false
@@ -128,8 +156,8 @@ export class Main extends React.Component<MainProps, State, DevicesList> {
                 'Authorization': 'Bearer ' + this.getTokenFromCookie()
             }
         }).then((res) => res.text()).then((res) => {
-            const resData = JSON.parse(res)
-            if (resData.error) {
+            const resData = res.length > 0 ? JSON.parse(res) : null
+            if (resData && resData.error) {
                 this.setState({ isAuthorized: false })
                 this.setState({ gotPlayerInfo: true })
                 return false
@@ -138,11 +166,11 @@ export class Main extends React.Component<MainProps, State, DevicesList> {
                 this.setState({ gotPlayerInfo: true })
                 return false
             }
-
-            const {id, name, is_active } = this.findActiveDevice(resData.devices)
+            const {id, name, is_active, volume_percent } = this.findActiveDevice(resData.devices)
             this.setState({ playerID: id.toString() })
             this.setState({ playerName: name })
             this.setState({ isActive: is_active })
+            this.setState({ volume: volume_percent})
             return true
         })
     }
@@ -156,13 +184,8 @@ export class Main extends React.Component<MainProps, State, DevicesList> {
                 'Authorization': 'Bearer ' + this.getTokenFromCookie()
             }
         }).then((res) => res.text()).then((res) => {
-            if (res.length === 0) {
-                this.setState({ gotPlayerInfo: true })
-                return false
-            }
-            const resData = JSON.parse(res)
-
-            if (resData.error) {
+            const resData = res.length > 0 ? JSON.parse(res) : null
+            if (resData && resData.error) {
                 this.setState({ isAuthorized: false })
                 this.setState({ gotPlayerInfo: true })
                 return false
@@ -183,6 +206,7 @@ export class Main extends React.Component<MainProps, State, DevicesList> {
         for (const a of data.item.artists) {
             artists += `${a.name} & `
         }
+
         // tslint:disable-next-line: object-literal-shorthand
         this.setState({isPlaying: isPlaying})
         // tslint:disable-next-line: object-literal-shorthand
@@ -197,9 +221,93 @@ export class Main extends React.Component<MainProps, State, DevicesList> {
         this.setState({album: album })
     }
 
+    /**
+     * Player commands
+     */
+    togglePlayer = () => {
+        const newState = this.state.isPlaying ? false : true
+        this.setState({ isPlaying: newState})
+
+        if (!this.state.isPlaying) {
+            fetch('https://api.spotify.com/v1/me/player/play', {
+                method: 'PUT',
+                headers: {
+                    'Authorization': 'Bearer ' + this.getTokenFromCookie()
+                }
+            }).then((res) => res.text()).then((res) => {
+                const resData = res.length > 0 ? JSON.parse(res) : null
+                if (resData && resData.error) {
+                    this.setState({ isAuthorized: false })
+                    return false
+                }
+            })
+        } else {
+            if (this.state.isPlaying) {
+                fetch('https://api.spotify.com/v1/me/player/pause', {
+                    method: 'PUT',
+                    headers: {
+                        'Authorization': 'Bearer ' + this.getTokenFromCookie()
+                    }
+                }).then((res) => res.text()).then((res) => {
+
+                    const resData = res.length > 0 ? JSON.parse(res) : null
+                    if (resData && resData.error) {
+                        this.setState({ isAuthorized: false })
+                        return false
+                    }
+                })
+            }
+        }
+    }
+
+    playerRepeat = () => {
+        return
+    }
+
+    nextSong = (allow: boolean = true) => {
+        if (!allow) {
+            return false
+        }
+        return fetch(`https://api.spotify.com/v1/me/player/next`, {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + this.getTokenFromCookie()
+            }
+        }).then((res) => res.text()).then((res) => {
+            const resData = res.length > 0 ? JSON.parse(res) : null
+            if (resData && resData.error) {
+                this.setState({ isAuthorized: false })
+                return false
+            }
+            setTimeout(this.getPlayingTrack, 200)
+            return true
+        })
+    }
+
+    prevSong = (allow: boolean = true) => {
+        if (!allow) {
+            return false
+        }
+        return fetch(`https://api.spotify.com/v1/me/player/previous`, {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + this.getTokenFromCookie()
+            }
+        }).then((res) => res.text()).then((res) => {
+            const resData = res.length > 0 ? JSON.parse(res) : null
+            if (resData && resData.error) {
+                this.setState({ isAuthorized: false })
+                return false
+            }
+            setTimeout(this.getPlayingTrack, 200)
+            return true
+        })
+    }
+
     checkAuthAndPlayer = () => {
         this.isUserLoggedIn().
         then(this.getDevice).
+        then(this.getShuffleAndRepeat).
         then(this.getPlayingTrack).
         then(() => {
             this.setState({ gotPlayerInfo: true })
@@ -210,6 +318,7 @@ export class Main extends React.Component<MainProps, State, DevicesList> {
         this.checkAuthAndPlayer()
     }
 
+    /** !!! */
     shouldComponentUpdate(a: any, b: any) {
         if (b.gotPlayerInfo === true && b.didCheckAuthState === true) {
             return true
@@ -254,6 +363,16 @@ export class Main extends React.Component<MainProps, State, DevicesList> {
                 isAuthorized={this.state.isAuthorized}
                 isPlaying={this.state.isPlaying}
                 currentlyPlaying={this.state.currentlyPlaying}
+                album={this.state.album}
+                artists={this.state.artists}
+                progress={this.state.progress}
+                duration={this.state.duration}
+                volume={this.state.volume}
+                updaterFn={this.checkAuthAndPlayer}
+                togglePlayerFn={this.togglePlayer}
+                repeatFn={this.playerRepeat}
+                nextFn={this.nextSong}
+                prevFn={this.prevSong}
                 />
             )
         }
--- src/player.tsx
+++ src/player.tsx
@@ -1,29 +1,98 @@
 import * as React from 'react'
+const devicesIcon = require('./images/devices.svg')
+const playIcon = require('./images/play.svg')
+const pauseIcon = require('./images/pause.svg')
+const shuffleIcon = require('./images/shuffle.svg')
+const repeatIcon = require('./images/repeat.svg')
+const nextIcon = require('./images/next.svg')
+const prevIcon = require('./images/previous.svg')
 
-interface PlayerProps {
+/**
+ * Object type for fn?
+ */
+export interface PlayerProps {
     isActive: boolean,
     isAuthorized: boolean,
     isPlaying: boolean,
     currentlyPlaying: string,
+    album: string,
+    artists: string,
+    progress: number,
+    duration: number,
+    volume: number,
+    updaterFn: any,
+    togglePlayerFn: any,
+    repeatFn: any,
+    nextFn: any,
+    prevFn: any,
 }
 
 export class Player extends React.Component<PlayerProps, {}> {
     render() {
+        // const updateInterval = () => setInterval(() => {
+        //     this.props.updaterFn()
+        // }, 30000)
+
+        /**
+         * Too many calls to API
+         * SOLUTION (??): SetTimeout of (DURATION-PROGRESS), then update
+         */
+        // updateInterval()
+
         if (!this.props.isActive) {
             return (
                 <div className='hero'>
-                    <div className='hero-item' style={{width: '100%', height: '80%'}}>
+                    <div className='hero-item'>
                         <h1>No devices connected</h1>
+                        <div className='round-icon'>
+                            <img src={devicesIcon} alt='' />
+                        </div>
                     </div>
                 </div>
             )
         }
-
+        const iconToUseForPlay = this.props.isPlaying === true ? pauseIcon : playIcon
         return (
             <div className='hero'>
                 <div className='hero-item' style={{width: '100%', height: '80%'}}>
-                    123
+                    <div className='hero' style={{width: '40%', position: 'absolute'}}>
+                        <div className='hero-item'>
+                            <div className='round-icon pointer' onClick={this.props.togglePlayerFn}>
+                                <img src={iconToUseForPlay} alt='' />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='hero' style={{width: '50%', position: 'absolute', left: '50%'}}>
+                        <div className='hero-item'>
+                            <p>{this.props.artists  }</p>
+                            <h1>{this.props.currentlyPlaying}</h1>
+                            <img src={prevIcon}
+                            alt='Prethodna pjesma'
+                            className='small-icon pointer'
+                            onClick={this.props.prevFn}
+                            />
+                            <img src={nextIcon}
+                            alt='Sljedeća pjesma'
+                            className='small-icon pointer'
+                            onClick={this.props.nextFn} />
+                            <img src={repeatIcon} alt='Ponavljaj pjesmu' className='small-icon pointer' />
+                            <img src={shuffleIcon} alt='Nasumična pjesma' className='small-icon pointer' />
+                        </div>
+                    </div>
+                    <div id='volume-slider-container'
+                        className='hero pointer'
+                        style={{width: '10%', position: 'absolute', left: '90%', padding: '18px 0'}}
+                    >
+                        <div id='volume-slider'>
+                            <div id='volume-slider-handle'
+                            style={{top: (100 - this.props.volume) + '%'}}
+                            >
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <div id='progress-bar' style={{width: (this.props.progress / this.props.duration) * 100 + '%'}}></div>
             </div>
         )
     }
