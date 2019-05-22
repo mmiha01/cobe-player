180848d9ade37baf5c2d9c8878d604e6b62d382e
mmiha01 <m.mihaljevic97@gmail.com>
2019-05-22 10:45:55 +0200

    text percentage show on hover

--- src/components/player/parts/Slider.tsx
+++ src/components/player/parts/Slider.tsx
@@ -10,7 +10,9 @@ export class Slider extends React.Component<SliderProps, {}> {
             <div id='volume-slider-container'
             className='hero abs-pos-hero right-side-hero pointer'>
             <div id='volume-slider'>
-                <div id='volume-slider-handle' style={{top: (100 - this.props.volume) + '%'}}></div>
+                <div id='volume-slider-handle' style={{top: (100 - this.props.volume) + '%'}}>
+                    <div id='volume-text-display'><p>{(100 - this.props.volume) + '%'}</p></div>
+                </div>
             </div>
         </div>
         )
