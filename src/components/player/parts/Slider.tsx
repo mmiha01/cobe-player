d6b636988535e3733a27b3a126c6a45dcba9cbd2
mmiha01 <m.mihaljevic97@gmail.com>
2019-05-22 10:48:16 +0200

    small slider fixes

--- src/components/player/parts/Slider.tsx
+++ src/components/player/parts/Slider.tsx
@@ -11,7 +11,7 @@ export class Slider extends React.Component<SliderProps, {}> {
             className='hero abs-pos-hero right-side-hero pointer'>
             <div id='volume-slider'>
                 <div id='volume-slider-handle' style={{top: (100 - this.props.volume) + '%'}}>
-                    <div id='volume-text-display'><p>{(100 - this.props.volume) + '%'}</p></div>
+                    <div id='volume-text-display'><p>{this.props.volume + '%'}</p></div>
                 </div>
             </div>
         </div>
