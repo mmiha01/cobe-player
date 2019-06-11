814d23a04e31d0b8835acdb069d4b82962d5c0ba
mmiha01 <m.mihaljevic97@gmail.com>
2019-06-11 13:54:01 +0200

    remove un-needed

--- src/services/Network.ts
+++ src/services/Network.ts
@@ -31,14 +31,4 @@ export class NetworkService {
             return res.length > 0 ? JSON.parse(res) : null
         })
     }
-
-    static checkWaitTime = () => {
-        return fetch('https://api.spotify.com/v1', {
-            headers: {
-                'Authorization': 'Bearer ' + CookieService.getTokenFromCookie()
-            },
-        }).then((res: Response) => {
-            return res.headers.get('Retry-After')
-        })
-    }
 }
